a75cc221abe53976150ea913878b857812794c6c
weberfern <weber12@gmail.com>
2023-12-18 22:28:40 -0300

    Inicio do projeto

---
+++ src/main/java/br/com/alura/screenmatch2/Screenmatch2Application.java
@@ -0,0 +1,27 @@
+package br.com.alura.screenmatch2;
+
+import br.com.alura.screenmatch2.model.DadosSerie;
+import br.com.alura.screenmatch2.service.ConsumoAPI;
+import br.com.alura.screenmatch2.service.ConverteDados;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Screenmatch2Application implements CommandLineRunner {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(Screenmatch2Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		var consumoApi = new ConsumoAPI();
+		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=friends&apikey=b383d2b0");
+		System.out.println(json);
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
+	}
+}
---
+++ src/test/java/br/com/alura/screenmatch2/Screenmatch2ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.screenmatch2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Screenmatch2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
